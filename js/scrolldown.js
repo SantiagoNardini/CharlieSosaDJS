2991955ec68cefed540861a1f621ede98d606896
Santiago Nardini <nardinisantiago@gmail.com>
2024-02-07 04:28:27 -0300

    javascript code optimized

--- js/scrolldown.js
+++ js/scrolldown.js
@@ -1,6 +1,6 @@
-$(function() {
-    $('.scroll-down').click (function() {
-      $('html, body').animate({scrollTop: $('footer').offset().top }, 'slow');
-      return false;
-    });
+document.querySelector(function() {
+  document.querySelector('.scroll-down').click (function() {
+    document.querySelector('html, body').animate({scrollTop: document.querySelector('footer').offset().top}, 'slow');
+    return false;
   });
+});
